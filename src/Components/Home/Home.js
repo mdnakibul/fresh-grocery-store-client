e835c1c274e05f006f07de234f86fbb90ebe9e9b
MdNakibulHosenNahid <56796558+MdNakibulHosenNahid@users.noreply.github.com>
2021-04-07 21:38:08 -0600

    Add spinner , update readme.md

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Product from '../Product/Product';
+import './Home.css'
 const Home = () => {
-    const [products,setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://safe-tundra-00266.herokuapp.com/products')
-        .then(res => res.json())
-        .then(data =>{
-            setProducts(data)
-        })
-        .catch(error => console.log(error))
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data);
+                document.getElementById('loader').style.display = 'none';
+            })
+            .catch(error => console.log(error))
+    }, [])
     return (
         <section>
             <Container className="mt-5">
                 <Row>
                     <div className="input-group mb-3">
                         <div className="form-outline">
-                            <input type="search" id="form1" className="form-control" placeholder="Seach..."/>
+                            <input type="search" id="form1" className="form-control" placeholder="Seach..." />
                         </div>
                         <button type="button" className="btn btn-primary">
                             Search
                         </button>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="lds-ripple" id='loader'>
+                        <div></div>
+                        <div></div>
                     </div>
                 </Row>
                 <Row>
